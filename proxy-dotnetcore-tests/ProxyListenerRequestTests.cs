a1891262f093233db6b8e4aa7eccd477f60e2297
gielow <andre.gielow@gmail.com>
2019-03-04 21:49:41 -0700

    Removed unused code and test cases. Added test cases for POST operations. Published application for both windows and ubuntu

--- proxy-dotnetcore-tests/ProxyListenerRequestTests.cs
+++ proxy-dotnetcore-tests/ProxyListenerRequestTests.cs
@@ -1,7 +1,5 @@
 using Microsoft.VisualStudio.TestTools.UnitTesting;
-using Newtonsoft.Json;
 using proxy_dotnetcore;
-using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Net;
